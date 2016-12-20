dd2b6a9336a4679a8786a15fb4cfb6987f604215
ccienfall <ccj2612@mail.ustc.edu.cn>
2016-12-20 20:47:28 +0800

    correct code

--- SVM/svm.py
+++ SVM/svm.py
@@ -1,21 +1,18 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
 
-def Dot(x1, x2):
-        '''
-    
-        '''
-    #garantee it is a inner product of two 1-dimension vector.
-    assert len( x1.shape ) == 1
-    assert len( x2.shape ) == 1
-    
-    return np.dot(x1,x2)
+def Dot( x1, x2 ):
+    assert len(x1.shape) == 1
+    assert len(x2.shape) == 1
+    return np.dot(x1, x2)
 
 def linearData( size = (100,2) ):
     data = 100 * np.random.random(size) - 50
     label = (np.dot( data, np.array([1,0.3]) ) + 0.2 ) > 0 
     label = label * 2 - 1
     return data,label
-    
+
 class svm(object):
 
     def __init__(self, kernelMethod=Dot, dataSource=linearData,eps = 1e-3, tol = 1e-3, C = 1 ):
@@ -27,7 +24,7 @@ class svm(object):
         self.dataSource = dataSource
         self.tol = tol
         self.C = C
-    def _init_w(self, size, force = False):
+    def _init_w(self, size = 2 , force = False):
         '''
         initialize w.
         '''
@@ -43,7 +40,7 @@ class svm(object):
             raise ValueError('b already exists, If you want to re-initialize b, please set force True.')
         self.b = 0.2 * np.random.random(1)
         
-    def _init_lm(self, size, force=False):
+    def _init_lm(self, size = 100 , force=False):
         '''
         initialize lagrange multipliers.
         '''
@@ -61,11 +58,17 @@ class svm(object):
         return self.lm[i]
     
     def target(self, i):
-        return self.data[i]
+        return self.label[i]
     
     def output(self, i):
-        return np.dot(self.w,self.data[i]) + self.b
+        #print(self.w.shape, self.data.shape,i)
         
+        #a = np.dot(self.w,self.data[i])
+        #return ( a  + self.b )[0]
+        if hasattr(self,'E') == False:
+            self.E = np.dot(self.data, self.w) - self.b
+        return self.E[i]
+
     def point(self, i):
         return self.data[i]
         
@@ -90,19 +93,27 @@ class svm(object):
         return phi_L, phi_H
        
     def notAtBound(self, alpha):
-        if alpha != 0 and alpha != self.C :
+        if alpha > 0 and alpha < self.C :
             return True
         else:
             return False
-    
+    def notOnBound(self, alpha):
+        if abs(alpha - 0)<self.tol and abs(alpha - self.C) < self.tol :
+            return True
+        else:
+            return False
+             
     def updateThreshold(self, alpha1, alpha2, E1, E2, y1, y2, k11, k12, k22, a1, a2):
         b1 = E1 + y1 * ( a1 - alpha1 ) * k11 + y2 * ( a2 - alpha2 ) * k12 + self.b
         b2 = E2 + y1 * ( a1 - alpha1 ) * k12 + y2 * ( a2 - alpha2 ) * k22 + self.b
         if self.notAtBound(alpha1) and self.notAtBound(alpha2) :
+            print(1)
             self.b = b1  
         elif not self.notAtBound(alpha1) and not self.notAtBound(alpha2) :
+            print(2)
             self.b = ( b1 + b2 ) / 2.0
         elif not self.notAtBound(alpha1) and self.notAtBound(alpha2):
+            print(3)
             self.b = b2
         else:
             self.b = b1
@@ -111,11 +122,12 @@ class svm(object):
     
         self.w = self.w + y1 * ( a1 - alpha1 ) * self.point(i1) + y2 * ( a2 - alpha2 ) * self.point(i2)
     
-    def updateErrorCache():
-    
+    def updateErrorCache(self):
+        
+        self.E = np.dot( self.data, self.w) - self.b
         
         
-    def updataLM(self,i1, i2, a1, a2):
+    def updateLM(self,i1, i2, a1, a2):
         self.lm[i1] = a1
         self.lm[i2] = a2
         
@@ -133,7 +145,7 @@ class svm(object):
         E1 = self.output(i1) - y1
         E2 = self.output(i2) - y2
         s = y1 * y2
-        L,H = getLH(alpha1, alpha2, y1, y2)
+        L,H = self.getLH(alpha1, alpha2, y1, y2)
         #if 
         if ( L == H):
             return 0
@@ -162,12 +174,17 @@ class svm(object):
         self.updateWeight(alpha1, alpha2, y1, y2, i1, i2, a1, a2)
         self.updateErrorCache()
         self.updateLM(i1,i2,a1,a2)
+        print(self.b)
+        return 1
         
     def numOfNonBound(self):    
         return ( ( self.lm != 0 ) * (self.lm != self.C ) > 0 ).sum()
         
-    def SecondChoiceHeuristic(self):
-    
+    def secondChoiceHeuristic(self,i):
+        if self.output(i) > 0:
+            return self.E.argmin()
+        else:
+            return self.E.argmax()
     
     def randomOrder(self):
         a = np.array(range(len(self.lm)))
@@ -176,19 +193,25 @@ class svm(object):
         
     def randomNonBoundOrder(self):
         a = self.randomOrder()
-        b = [i for i in a if self.notAtBound(i)]
+        b = [i for i in a if self.notOnBound(i)]
         return b
-        
+    
+    def NonBoundOrder(self):
+        a = np.array(range(len(self.lm)))
+        b = [i for i in a if self.notOnBound(i)]
+        return b
    
     
-    def examineExample(i2):
+    def examineExample(self,i2):
         y2 = self.target(i2)
         alpha2 = self.getLM(i2)
+        #print(self.output(i2))
         E2 = self.output(i2) - y2
         r2 = E2 * y2
+        #print(r2)
         if ( ( r2 < -self.tol and alpha2 < self.C) ) or ( ( r2 > self.tol and alpha2 > 0) ) :
             if ( self.numOfNonBound() > 1 ):
-                i1 = self.secondChoiceHeuristic()
+                i1 = self.secondChoiceHeuristic(i2)
                 if ( self.takeStep(i1,i2) ):
                     return 1
             for i1 in self.randomNonBoundOrder():
@@ -199,21 +222,47 @@ class svm(object):
                     return 1
         return 0
 
-    def train():
+    def train(self):
+        self._init_w()
+        self._init_b()
+        self._init_lm()
+        self.getData()
+        #self.Visualize(f=2)
         numChanged = 0
         examineAll = 1
         while( numChanged > 0 or examineAll ):
             numChanged = 0
             if examineAll:
-                for I in self.data:
+                for I in range(len(self.lm)):
                     numChanged += self.examineExample(I)
             else:
-                for I in self.NonBound():
-                    numchanged += self.examineExample(I)
+                #print(self.NonBoundOrder())
+                for I in self.NonBoundOrder():
+                    numChanged += self.examineExample(I)
             if examineAll == 1 :
                 examineAll = 0
             elif numChanged == 0 :
                 examineAll = 1
                 
-            
+    def evaluate(self):
+        return ( ( ( np.dot(self.data,self.w) - self.b ).reshape(100) > 0 ) != (self.label > 0 ) ) .sum()
         
+    def Visualize(self,f = 1, saveImage = True):
+        fg = plt.figure(f)
+        idx_1 = self.label==1
+        p1 = plt.scatter(self.data[idx_1,1], self.data[idx_1,0],marker='x', color = 'm', label = '1',s=30)
+        idx_2 = self.label==-1
+        p2 = plt.scatter(self.data[idx_2,1], self.data[idx_2,0],marker='+', color = 'c', label = '2',s=50)
+        p3 = plt.plot([50,-50],[-0.3*50-0.2,0.3*50-0.2 ],color = 'b')
+        if f != 1:
+            p4 = plt.plot([50,-50],[(-50*self.w[1]+self.b)/self.w[0],(50*self.w[1]+self.b)/self.w[0]],color = 'r')
+        if saveImage == True:
+            plt.savefig(str(f)+'.jpg',format='jpg')
+        else:
+            plt.show()        
+if __name__ == '__main__':
+    s =svm(C=10000)
+    s.train()
+    print s.evaluate()
+    print(s.w/s.w[0],s.b/s.w[0])
+    s.Visualize(f=3,saveImage=False)
